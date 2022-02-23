96757ab0f50126fc24dd7722b4471d04653a77dd
silmarsendin <38678264+silmarsendin@users.noreply.github.com>
2022-02-22 23:05:24 -0300

    Add files via upload

--- appsendin.py
+++ appsendin.py
@@ -68,7 +68,7 @@ elif paginaselecionada == 'Tanques em Edificações':
     classeliq = st.selectbox('Qual a Classe do Líquido Armazenado:',['Classe I','Classe IA','Classe IB','Classe IC','Classe II','Classe IIIA','Classe IIIB'])
     volumetanques = st.number_input("Qual o volume total, em litros, armazenado sem isolamento:")
     if volumetanques < 20001 or classeliq == 'Classe IIIB':
-        st.write('Não será necessário os sistemas de espuma e resfriamento')
+        st.write('Não será necessário os sistemas de espuma e resfriamento de acordo com a NBR 17.505 mas pela IT 25/19 deverá ser previsto para IIIB com mais de 20 m3.')
         st.write('Prever Sistema de Extintor de acordo com a Tabela 1.2 da IT 25/19 SP ou Tabela A.11 da NBR 17.505 - Parte 7.')
     elif volumetanques > 20000 or classeliq != 'Classe IIIB':
         mostrarnbr = st.checkbox('Quero saber como proteger o cenário de acordo com a NBR 17.505.')
@@ -84,6 +84,42 @@ elif paginaselecionada == 'Tanques em Edificações':
             st.write('Prever Sistema de Espuma de acordo com o item o item 7.2.2.1.1. da Instrução Técnica 25/19 do Corpo de Bombeiros de São Paulo.')
             st.write('Prever Sistema de Resfriamento de acordo com o item o item 7.4 da Instrução Técnica 25/19 do Corpo de Bombeiros de São Paulo.')
             st.write('Prever Sistema de Contenção de acordo com o item 2.3.7. da Instrução Técnica 25/19 do Corpo de Bombeiros de São Paulo.')
+        mostrarcurso = st.checkbox('Quero saber mais sobre proteção contra incêndio para líquidos igníferos.')
+        if mostrarcurso:
+            st.write('Visite: https://www.silmarsendin.com/cursos-on-line-1 ')
+
+            #Exigências para Fracionados.
+elif paginaselecionada == 'Fracionados':
+    st.title('Líquidos Fracionados')
+    st.write('O cenário selecionado é um armazenamento de líquidos fracionados e as informações a seguir só se aplicam a este cenário.')
+
+    mostrarclasses = st.checkbox('Quero saber como classificar um líquido ignífero.')
+    if mostrarclasses:
+        from PIL import Image
+        image = Image.open('classes.png')
+        st.image(image, caption='Classes dos Líquidos Igníferos')
+        st.write('PF = Ponto de Fulgor, PV = Pressão de Vapor, e PE = Ponto de Ebulição')
+
+    classeliq = st.selectbox('Qual a Classe do Líquido Armazenado:',['Classe I','Classe IA','Classe IB','Classe IC','Classe II','Classe IIIA','Classe IIIB'])
+    volumetanques = st.number_input("Qual o volume total, em litros, armazenado sem isolamento:")
+    if volumetanques < 20001 or classeliq == 'Classe IIIB':
+        st.write('Não será necessário os sistemas de espuma e resfriamento de acordo com a NBR 17.505 mas pela IT 25/19 deverá ser previsto para IIIB com mais de 20 m3.')
+        st.write('Prever Sistema de Extintor de acordo com a Tabela 1.2 da IT 25/19 SP ou Tabela A.11 da NBR 17.505 - Parte 7.')
+    elif volumetanques > 20000 or classeliq != 'Classe IIIB':
+       
+        mostrarit25 = st.checkbox('Quero saber como proteger o cenário de acordo com a IT 25/19 de SP.')
+        if mostrarit25:
+            st.write('Prever Sistema de Espuma de acordo com a Tabela 4.25 para áreas abertas e 4.27 para áreas fechadas da Instrução Técnica 25/19 do Corpo de Bombeiros de São Paulo.')
+            st.write('Prever Sistema de Resfriamento de acordo com a Tabela 4.26 para áreas abertas e 4.28 para áreas fechadas da Instrução Técnica 25/19 do Corpo de Bombeiros de São Paulo.')
+            st.write('Prever Sistema de Contenção de acordo com o item 4.8 da Instrução Técnica 25/19 do Corpo de Bombeiros de São Paulo.')
+            st.write('Caso os volumes armazenados ultrapassem os determinados na Tabela 4.9, prever Sistema de Chuveiros automáticos de acordo com o item 4.20 da Instrução Técnica 25/19 do Corpo de Bombeiros de São Paulo.')
+            mostrar49 = st.checkbox('Quero ver a Tabela 4.9.')
+            if mostrar49:
+                from PIL import Image
+                image2 = Image.open('tabela49.png')
+                st.image(image2, caption='Tabela 4.9: Quantidades máximas para armazéns de líquidos (M-2) sem sistema de chuveiros automáticos')
+                
+
         mostrarcurso = st.checkbox('Quero saber mais sobre proteção contra incêndio para líquidos igníferos.')
         if mostrarcurso:
             st.write('Visite: https://www.silmarsendin.com/cursos-on-line-1 ')
